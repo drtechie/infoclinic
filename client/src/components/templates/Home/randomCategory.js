53b2acee0d4888364ed1e88c4611416c94c23ddc
Mithun James <mithun@grp.avegenhealth.com>
2018-11-10 15:38:26 +0530

    Add category section and API calls

--- client/src/components/templates/Home/randomCategory.js
+++ client/src/components/templates/Home/randomCategory.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import './randomCategory.scss'
+import { faHeart, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { connect } from 'react-redux';
 import Swiper from "swiper";
+import './randomCategory.scss'
+import api from "../../../api";
 
-export default class RandomCategory extends Component {
+const mapStateToProps = (state) => ({
+    categoriesList: state.api.lists.categories,
+});
+
+class RandomCategory extends Component {
     constructor(props){
         super(props);
         this.categorySwiperContainer = React.createRef();
+        this.state = {
+            posts: [],
+        };
     }
 
     initializeSlider() {
@@ -23,95 +32,100 @@ export default class RandomCategory extends Component {
         }
     }
 
+    fetchRandomCategory() {
+        if (this.props.categoriesList && this.props.categoriesList.length > 0) {
+            const atLeastFivePosts = this.props.categoriesList.filter(category => category.count >= 5);
+            if (atLeastFivePosts.length > 0) {
+                const category = atLeastFivePosts[Math.floor(Math.random()*atLeastFivePosts.length)];
+                api.Content.postsByCategory(category.id, 1, 5).then(
+                    res => {
+                        this.setState({posts: res});
+                        this.initializeSlider();
+                    },
+                    error => {
+                        console.warn(error);
+                    }
+                );
+            }
+
+        }
+    }
+
     componentDidMount() {
-        this.initializeSlider();
+        this.fetchRandomCategory();
     }
 
     componentDidUpdate(prevProps) {
-        if (prevProps.data !== this.props.data) {
-            this.initializeSlider();
+        if (prevProps.categoriesList !== this.props.categoriesList) {
+            this.fetchRandomCategory();
         }
     }
 
     render() {
-        return (
-            <section id="top-stories" className="row" key='category-random'>
-                <div className="sub-title title">
-                    <h4>Today’s top stories</h4>
-                    <a href="/">More <FontAwesomeIcon icon={faHeart} /></a>
-                </div>
-                <div className="container-stories clearfix">
-                    <div className="wrapper-post ">
-                        <div className="wrap post-type-swiper" ref={this.categorySwiperContainer}>
-                            <div className="swiper-wrapper">
-                                <article className="swiper-slide post-swiper overlay" style={{backgroundImage: "url('https://place-hold.it/763x563')" }}>
-                                    <div className="by-writer">
-                                        <div className="icon-author" style={{backgroundImage: "url('https://place-hold.it/63x63')" }}>
-                                        </div>
-                                        <div className="info-author">
-                                            <div className="name"><a href="/">William Wright</a>
-                                            </div>
-                                            <time className="data" dateTime="2017-07-20"> Jan 20, 2017</time>
-                                            <span className="timetoread"> · 5 min to read</span>
-                                        </div>
-                                    </div>
-                                    <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
-                                    <h2 title="Why every travel photographer should carry a notebook">
-                                        <a href="/">
-                                            Why every travel photographer should carry a notebook
-                                        </a>
-                                    </h2>
-                                </article>
-                                <article className="swiper-slide post-swiper overlay" style={{backgroundImage: "url('https://place-hold.it/763x563')" }} >
-                                    <div className="by-writer">
-                                        <div className="icon-author" style={{backgroundImage: "url('https://place-hold.it/63x63')" }}>
-                                        </div>
-                                        <div className="info-author">
-                                            <div className="name"><a href="/">Mario Lewis</a></div>
-                                            <time className="data" dateTime="2017-07-20"> Jan 20, 2017</time>
-                                            <span className="timetoread"> · 5 min to read</span>
-                                        </div>
-                                    </div>
-                                    <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
-                                    <h2 title="Why every travel photographer should carry a notebook">
-                                        <a href="/">
-                                            Why every travel photographer should carry a notebook
-                                        </a>
-                                    </h2>
-                                </article>
-                                <article className="swiper-slide post-swiper overlay"  style={{backgroundImage: "url('https://place-hold.it/763x563')" }}>
-                                    <div className="by-writer">
-                                        <div className="icon-author" style={{backgroundImage: "url('https://place-hold.it/63x63')" }}>
-                                        </div>
-                                        <div className="info-author">
-                                            <div className="name"><a href="/">Mario Lewis</a></div>
-                                            <time className="data" dateTime="2017-07-20"> Jan 20, 2017</time>
-                                            <span className="timetoread"> · 5 min to read</span>
-                                        </div>
-                                    </div>
-                                    <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
-                                    <h2 title="Why every travel photographer should carry a notebook"><a
-                                        href="/">Why
-                                        every travel photographer should carry a notebook</a></h2>
-                                </article>
-                            </div>
-                            <div className="swiper-pagination horizontal"/>
-                        </div>
+        const { posts }= this.state;
+        if (posts.length > 0) {
+            return (
+                <section id="top-stories" className="row" key='category-random'>
+                    <div className="sub-title title">
+                        <h4>Today’s top stories</h4>
+                        <a href="/">More <FontAwesomeIcon icon={faChevronRight} /></a>
                     </div>
-                    <article className="wrapper-post post-image overlay"  style={{backgroundImage: "url('https://place-hold.it/379x263')" }}>
-                        <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
-                        <h2 title="Don’t date a girl who travels">
-                            <a href="/">Don’t date a girl who travels</a>
-                        </h2>
-                    </article>
-                    <article className="wrapper-post post-image overlay"  style={{backgroundImage: "url('https://place-hold.it/379x263')" }}>
-                        <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
-                        <h2 title="Don’t date a girl who travels"><a href="/">Don’t date a girl who
-                            travels</a></h2>
-                    </article>
-                    <div className="holder">article of the day</div>
-                </div>
-            </section>
-        );
+                    <div className="container-stories clearfix">
+                        <div className="wrapper-post ">
+                            <div className="wrap post-type-swiper" ref={this.categorySwiperContainer}>
+                                <div className="swiper-wrapper">
+                                    {
+                                        posts.slice(0, 3).map(post => {
+                                            return (
+                                                <article
+                                                    className="swiper-slide post-swiper overlay"
+                                                    style={{backgroundImage: "url('https://place-hold.it/763x563')" }}
+                                                    key={post.id}
+                                                >
+                                                    <div className="by-writer">
+                                                        <div className="icon-author" style={{backgroundImage: "url('https://place-hold.it/63x63')" }}>
+                                                        </div>
+                                                        <div className="info-author">
+                                                            <div className="name"><a href="/">William Wright</a>
+                                                            </div>
+                                                            <time className="data" dateTime="2017-07-20"> Jan 20, 2017</time>
+                                                            <span className="timetoread"> · 5 min to read</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
+                                                    <h2 title="Why every travel photographer should carry a notebook">
+                                                        <a href="/">
+                                                            {post.title.rendered}
+                                                        </a>
+                                                    </h2>
+                                                </article>
+                                            )
+                                        })
+                                    }
+                                </div>
+                                <div className="swiper-pagination horizontal"/>
+                            </div>
+                        </div>
+                        <article className="wrapper-post post-image overlay"  style={{backgroundImage: "url('https://place-hold.it/379x263')" }}>
+                            <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
+                            <h2 title="Don’t date a girl who travels">
+                                <a href="/">{ posts[3].title.rendered }</a>
+                            </h2>
+                        </article>
+                        <article className="wrapper-post post-image overlay"  style={{backgroundImage: "url('https://place-hold.it/379x263')" }}>
+                            <div className="likes"><FontAwesomeIcon icon={faHeart} />43 109</div>
+                            <h2 title="Don’t date a girl who travels">
+                                <a href="/">{ posts[4].title.rendered }</a>
+                            </h2>
+                        </article>
+                        <div className="holder">article of the day</div>
+                    </div>
+                </section>
+            );
+        }
+        return null;
+
     }
 }
+
+export default connect(mapStateToProps)(RandomCategory);
