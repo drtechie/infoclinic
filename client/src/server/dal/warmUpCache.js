72b3c005c7610de3fefcb8fa042a2ad7db725d9e
Mithun James <mithun@grp.avegenhealth.com>
2019-02-24 19:59:17 +0530

    Add pages and categories to cache warm up

--- client/src/server/dal/warmUpCache.js
+++ client/src/server/dal/warmUpCache.js
@@ -1,17 +1,35 @@
 import api from "../../api";
 
 export const warmUpPostsCache = (store) =>  {
-    getPage(1, store);
+    getPostsPage(1, store);
+    getPageList(store);
+    getCateogoryList(store);
 };
 
-const getPage = (page, store) => {
+const getPostsPage = (page, store) => {
     api.Content.postsByPage(page, true, 100)
         .then((response) => {
-            console.log(`Caching page ${page}`);
+            console.log(`Caching posts page ${page}`);
             store.dispatch({ type: 'LOAD_DATA', payload: { type: 'posts', data: response.body } });
             if (page < parseInt(response.headers["x-wp-totalpages"], 10)) {
                 const newPage = page + 1;
-                getPage(newPage, store);
+                getPostsPage(newPage, store);
             }
         })
-}
+};
+
+const getPageList = (store) => {
+    api.Content.pageList()
+        .then((response) => {
+            console.log('Caching pages');
+            store.dispatch({ type: 'LOAD_PAGES_LIST', payload: response });
+        })
+};
+
+const getCateogoryList = (store) => {
+    api.Content.categoryList()
+        .then((response) => {
+            console.log('Caching categories');
+            store.dispatch({ type: 'LOAD_CATEGORIES_LIST', payload: response });
+        })
+};
